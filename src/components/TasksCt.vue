6c2064dc9aa3476d26befafb5b73c53bed8e0d4a
Vikas <kedia.vikas@gmail.com>
2024-12-21 17:12:49 -0800

    making sharing simpler

--- src/components/TasksCt.vue
+++ src/components/TasksCt.vue
@@ -165,7 +165,7 @@ export default {
       try {
         const { data: shares, error } = await supabase
           .from('matter_access')
-          .select('id, shared_with_user_id, access_type')
+          .select('shared_with_user_id, access_type')
           .eq('matter_id', this.currentMatter.id);
 
         if (error) throw error;

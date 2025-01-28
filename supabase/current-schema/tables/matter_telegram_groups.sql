62d659af6fb25a78b3ab849e861687681e8f6492
suvankar-grmtech <suvankar@grmtech.com>
2025-01-28 20:39:00 +0530

    added interface to map telegram group to a matter

---
+++ supabase/current-schema/tables/matter_telegram_groups.sql
@@ -0,0 +1,8 @@
+CREATE TABLE matter_telegram_groups (
+  id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+  matter_id bigint REFERENCES matters(id) ON DELETE CASCADE,
+  chat_id text NOT NULL,
+  name text NOT NULL,
+  notification_types text[] DEFAULT '{}',
+  created_at timestamp with time zone DEFAULT timezone('utc', now())
+);

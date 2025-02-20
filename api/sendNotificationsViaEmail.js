ac63aa44c1c79944141afa0c33acd3ca41fa2560
suvankar-grmtech <suvankar@grmtech.com>
2025-02-21 01:25:53 +0530

    working on read from notifications table and send email logic.

--- api/sendNotificationsViaEmail.js
+++ api/sendNotificationsViaEmail.js
@@ -36,7 +36,8 @@ export default async function handler(req, res) {
       console.log('First 5 notifications:', data);
 
       return res.status(200).json({ 
-        message: 'Notifications retrieved successfully'
+        message: 'Notifications retrieved successfully',
+        data: data
       });
     } catch (error) {
       console.error('Error fetching notifications:', error);
